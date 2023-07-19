9fc0ac20ee1cf4a9362d38ce0dedf728f0c1e050
Soumitra Saha <soumitrosahaofficial@gmail.com>
2023-07-19 11:25:45 +0530

    Submit Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import Button from "./Button.Component";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+	return (
+		<div>
+			{/* Do not remove the main div */}
+			<Button />
+		</div>
+	);
+};
 
-export default App
+export default App;
